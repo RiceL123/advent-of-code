98ab89f547da6b2e8dec48e43a423c14260ab312
RiceL123 <115810538+RiceL123@users.noreply.github.com>
2022-12-02 20:11:20 +1100

    :)

--- 2022/day2.py
+++ 2022/day2.py
@@ -3,8 +3,8 @@ import re
 dictionary = {'A': 'X', 'B': 'Y', 'C': 'Z', 
               'X': 1, 'Y': 2, 'Z': 3}
 
-# if you want to lose then (+ 1 % 3)
-# if you want to win then (if index = 0 then then choose 3, else -1 to index)
+# if you want to lose then (- 1 % 3)
+# if you want to win then (+1 % 3)
 # if you want to tie then keep the same
 array = ['A', 'B', 'C']
 
@@ -18,27 +18,22 @@ with open("day2.txt") as f:
     for line in f:
         
         letters = line.split()
-        #lose so + 1
+        # lose so - 1 % 3
         if letters[1] == 'X':
-            # print(letters, end = '')
             for i in range(3):
                 if array[i] == letters[0]:
                     letters[1] = dictionary[array[(i - 1) % 3]]
                     # print(f"{array[i]} -> lose -> {array[(i + 1) % 3]}: {letters[1]}")
         # draw
         elif letters[1] == 'Y':
-            #   # print(letters, end = '')
-            # print(f"{letters[0]} -> draw -> {letters[0]}: {dictionary[letters[0]]}")
             letters[1] = dictionary[letters[0]]
-        # win
+        # win so + 1 % 3
         elif letters[1] == 'Z':
-            #   # print(letters, end = '')
             for i in range(3):
                 if array[i] == letters[0]:
                     letters[1] = dictionary[array[(i + 1) % 3]]
                     # print(f"{array[i]} -> Win -> {array[(i - 1) % 3]}: {letters[1]}")
 
-        # print(letters)
         sum += dictionary[letters[1]]
         if (letter_dict[letters[1]] == letters[0]):
             sum += 3
