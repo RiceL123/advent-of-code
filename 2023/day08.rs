77f298aae4b45992affc47a568313ce1f2931ad6
Eric Liao <ericliao57@gmail.com>
2023-12-09 03:44:39 +1100

    lcm instead of brute force

--- 2023/day08.rs
+++ 2023/day08.rs
@@ -1,4 +1,5 @@
 use std::{fs::{File, self}, io::{BufReader, BufRead}, collections::HashMap, path::Path};
+
 fn main() {
     let path = Path::new("src/test.txt");
     let file = File::open(&path).expect("couldn't open file");
@@ -51,27 +52,69 @@ fn part1(instructions: &str, node_map: &HashMap<String, (String, String)>) -> u3
     steps
 }
 
-fn part2(instructions: &str, node_map: &HashMap<String, (String, String)>, starting_elements: &Vec<String>) -> u64 {
-    let mut steps: u64 = 0;
-
-    let mut elements = starting_elements.clone();
-    // println!("{:?}", elements);
-
-    loop {
-        for c in instructions.chars() {
-            steps += 1;
-
-            let mut i = 0;
-            while i < elements.len() {
-                match c {
-                    'L' => elements[i] = node_map.get(&elements[i]).expect("left key not found").0.clone(),
-                    'R' => elements[i] = node_map.get(&elements[i]).expect("right key not found").1.clone(),
-                    _ => { eprintln!("!instruction.match([LR])")}
-                }
-                i += 1
-            }
-
-            if elements.iter().all(|x| x.chars().last().expect("elements Vec is not filled") == 'Z') { return steps; }
-        }
+fn gcd(mut a: u64, mut b: u64) -> u64 {
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
     }
+    a
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    if a == 0 || b == 0 {
+        0
+    } else {
+        (a / gcd(a, b)) * b
+    }
+}
+
+fn part2(instructions: &str, node_map: &HashMap<String, (String, String)>, starting_elements: &Vec<String>) -> u64 {
+    let elements = starting_elements.clone();
+
+    elements
+        .iter()
+        .map(|x| {
+            let mut steps: u64 = 0;
+            let mut element = x.clone();
+
+            while element.chars().last().unwrap() != 'Z' {
+                for c in instructions.chars() {
+                    steps += 1;
+                    element = match c {
+                        'L' => node_map.get(&element).expect("left key not found").0.clone(),
+                        'R' => node_map.get(&element).expect("right key not found").1.clone(),
+                        _ => panic!()
+                    };
+                }
+            }
+        
+            steps
+        })
+        .reduce(|acc, e| lcm(acc, e))
+        .unwrap()
+
+    // the below solution will take very long to calculate despite being correct as answers for
+    // this day's problem are in the trillions; as such a more optimal solution is required (above)
+
+    // let mut steps: u64 = 0;
+
+    // let elements = starting_elements.clone();
+    // loop {
+    //     for c in instructions.chars() {
+    //         steps += 1;
+
+    //         let mut i = 0;
+    //         while i < elements.len() {
+    //             match c {
+    //                 'L' => elements[i] = node_map.get(&elements[i]).expect("left key not found").0.clone(),
+    //                 'R' => elements[i] = node_map.get(&elements[i]).expect("right key not found").1.clone(),
+    //                 _ => { eprintln!("!instruction.match([LR])")}
+    //             }
+    //             i += 1
+    //         }
+
+    //         if elements.iter().all(|x| x.chars().last().expect("elements Vec is not filled") == 'Z') { return steps; }
+    //     }
+    // }
 }   
