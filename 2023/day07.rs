9b76cf781a63fc5b473a4573426bab71ef585181
Eric Liao <ericliao57@gmail.com>
2023-12-21 13:57:21 +1100

    refactored day07

--- 2023/day07.rs
+++ 2023/day07.rs
@@ -16,9 +16,7 @@ pub fn part1(input: &str) -> u64 {
         })
         .collect::<Vec<(Hand, u64)>>();
 
-    cards.sort_by(|a, b| b.0.compare(&a.0));
-
-    // println!("{:?}", cards);
+    cards.sort_by(|a, b| b.0.compare(&a.0, "AKQJT98765432"));
 
     let sum = cards
         .iter()
@@ -39,15 +37,13 @@ pub fn part2(input: &str) -> u64 {
             let tuple = line.splitn(2, " ").collect::<Vec<&str>>();
 
             (
-                WildCardHand::new(tuple[0]),
+                Hand::new_wild_card(tuple[0]),
                 tuple[1].parse::<u64>().expect("bid amount = NaN"),
             )
         })
-        .collect::<Vec<(WildCardHand, u64)>>();
+        .collect::<Vec<(Hand, u64)>>();
 
-    cards.sort_by(|a, b| b.0.compare(&a.0));
-
-    // println!("{:?}", cards);
+    cards.sort_by(|a, b| b.0.compare(&a.0, "AKQT98765432J"));
 
     let sum = cards
         .iter()
@@ -105,124 +101,84 @@ fn calc_hand_type(cards: &str) -> HandType {
     hand_type
 }
 
+fn calc_wild_card_hand_type(cards: &str) -> HandType {
+    let face_order = "AKQT98765432J";
+    let wild_card: char = 'J';
+
+    if !cards.contains(wild_card) {
+        return calc_hand_type(cards)
+    }
+
+    let mut card_counts = HashMap::new();
+
+    for c in cards.chars() {
+        *card_counts.entry(c).or_insert(0) += 1;
+    }
+
+    let best_char = card_counts
+        .iter()
+        .max_by_key(|(_, val)| **val)
+        .expect("not a valid hand");
+
+    // if 5 different characters
+    if *best_char.1 == 1 {
+        for c in face_order.chars() {
+            // find the best card and replace the J with it
+            if cards.contains(c) {
+                let modified_cards = cards.replace(wild_card, &c.to_string());
+                return calc_hand_type(&modified_cards.clone())
+            }
+        }
+    } else if *best_char.0 == wild_card {
+        if *best_char.1 == 5 {
+            return calc_hand_type(cards)
+        }
+
+        // find the next best card and turn all the J's into it
+        let second_best_char = card_counts
+            .iter()
+            .filter(|(char, _)| **char != wild_card)
+            .max_by_key(|(_, val)| **val)
+            .expect("not a valid hand");
+
+        if *second_best_char.1 == 1 {
+            for c in face_order.chars() {
+                // find the best card and replace the J with it
+                if cards.contains(c) {
+                    let modified_cards = cards.replace(wild_card, &c.to_string());
+                    return calc_hand_type(&modified_cards.clone())
+                }
+            }
+        } else {
+            let modified_cards = cards.replace(wild_card, &second_best_char.0.to_string());
+            return calc_hand_type(&modified_cards.clone())
+        }
+    } else {
+        let modified_cards = cards.replace(wild_card, &best_char.0.to_string());
+        return calc_hand_type(&modified_cards.clone())
+    }
+
+    let res = calc_hand_type(cards);
+    res
+}
+
 impl<'a> Hand<'a> {
     fn new(cards: &'a str) -> Self {
         let hand_type = calc_hand_type(cards);
-
         Self(cards, hand_type)
     }
 
-    fn compare(&self, other: &Hand<'_>) -> Ordering {
+    fn new_wild_card(cards: &'a str) -> Self {
+        let hand_type = calc_wild_card_hand_type(cards);
+        Self(cards, hand_type)
+    }
+
+    fn compare(&self, other: &Hand<'_>, face_order: &str) -> Ordering {
         match self.1.cmp(&other.1) {
             Ordering::Equal => {
                 for i in 0..5 {
                     let self_first_letter = self.0.chars().nth(i).unwrap_or(' ');
                     let other_first_letter = other.0.chars().nth(i).unwrap_or(' ');
-                    let face_order = "AKQJT98765432";
-
-                    let self_face_value = face_order.find(self_first_letter);
-                    let other_face_value = face_order.find(other_first_letter);
-
-                    let res = self_face_value.cmp(&other_face_value);
-                    if res != Ordering::Equal {
-                        return res;
-                    }
-                }
-
-                return self.0.cmp(&other.0);
-            }
-            other => other,
-        }
-    }
-}
-
-struct WildCardHand<'a>(String, HandType, &'a str);
-
-impl<'a> WildCardHand<'a> {
-    fn new(cards: &'a str) -> Self {
-        let face_order = "AKQT98765432J";
-        let wild_card: char = 'J';
-
-        if !cards.contains(wild_card) {
-            return Self(cards.to_string(), calc_hand_type(cards), cards);
-        }
-
-        let mut card_counts = HashMap::new();
-
-        for c in cards.chars() {
-            *card_counts.entry(c).or_insert(0) += 1;
-        }
-
-        let best_char = card_counts
-            .iter()
-            .max_by_key(|(_, val)| **val)
-            .expect("not a valid hand");
-
-        // if 5 different characters
-        if *best_char.1 == 1 {
-            for c in face_order.chars() {
-                // find the best card and replace the J with it
-                if cards.contains(c) {
-                    let modified_cards = cards.replace(wild_card, &c.to_string());
-                    return Self(
-                        modified_cards.clone(),
-                        calc_hand_type(&modified_cards),
-                        cards,
-                    );
-                }
-            }
-        } else if *best_char.0 == wild_card {
-            if *best_char.1 == 5 {
-                return Self(cards.to_string(), calc_hand_type(cards), cards);
-            }
-
-            // find the next best card and turn all the J's into it
-            let second_best_char = card_counts
-                .iter()
-                .filter(|(char, _)| **char != wild_card)
-                .max_by_key(|(_, val)| **val)
-                .expect("not a valid hand");
-
-            if *second_best_char.1 == 1 {
-                for c in face_order.chars() {
-                    // find the best card and replace the J with it
-                    if cards.contains(c) {
-                        let modified_cards = cards.replace(wild_card, &c.to_string());
-                        return Self(
-                            modified_cards.clone(),
-                            calc_hand_type(&modified_cards.clone()),
-                            cards,
-                        );
-                    }
-                }
-            } else {
-                let modified_cards = cards.replace(wild_card, &second_best_char.0.to_string());
-                return Self(
-                    modified_cards.clone(),
-                    calc_hand_type(&modified_cards.clone()),
-                    cards,
-                );
-            }
-        } else {
-            let modified_cards = cards.replace(wild_card, &best_char.0.to_string());
-            return Self(
-                modified_cards.clone(),
-                calc_hand_type(&modified_cards.clone()),
-                cards,
-            );
-        }
-
-        let hand_type = calc_hand_type(cards);
-        return Self(cards.to_string(), hand_type, cards);
-    }
-
-    fn compare(&self, other: &WildCardHand) -> Ordering {
-        match self.1.cmp(&other.1) {
-            Ordering::Equal => {
-                for i in 0..5 {
-                    let self_first_letter = self.2.chars().nth(i).unwrap_or(' ');
-                    let other_first_letter = other.2.chars().nth(i).unwrap_or(' ');
-                    let face_order = "AKQT98765432J";
 
                     let self_face_value = face_order.find(self_first_letter);
                     let other_face_value = face_order.find(other_first_letter);
