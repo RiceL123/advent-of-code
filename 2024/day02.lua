48519d990d14f4fedc6bee111b75be2958aefe5a
RiceL123 <ericliao57@gmail.com>
2024-12-02 20:57:30 +1100

    🤷‍♀️

--- 2024/day02.lua
+++ 2024/day02.lua
@@ -1,5 +1,3 @@
--- cat day02.txt | lua day02.lua
-
 function table.copy(t)
     local t2 = {}
     for k,v in pairs(t) do
@@ -8,35 +6,43 @@ function table.copy(t)
     return t2
 end
 
--- returns the -1 everything is sweet
-function table.difference_not_in_bounds_index(array, lower_bound, upper_bound)
-    local arr = table.copy(array)
-    curr = table.remove(arr, 1)
-    for i, num in ipairs(arr) do
-        difference = curr - num
-        if not (difference >= lower_bound and difference <= upper_bound) then
-            return i
-        end
-        curr = num
+function line_to_report(line)
+    report = {}
+    for token in string.gmatch(line, "[^%s]+") do
+        table.insert(report, tonumber(token))
     end
-    return -1
+    return report
+end
+
+function is_safe(report)
+    increasing = true
+    for i=1,#report - 1 do
+        difference = report[i] - report[i + 1]
+        if not (difference >= 1 and difference <= 3) then
+            increasing = false
+            break
+        end
+    end
+
+    decreasing = true
+    for i=1,#report - 1 do
+        difference = report[i + 1] - report[i]
+        if not (difference >= 1 and difference <= 3) then
+            decreasing = false
+            break
+        end
+    end
+
+    return increasing or decreasing
 end
 
 function part1()
     io.input("./day02.txt")
     n_safe = 0
     for line in io.lines() do
-        report = {}
-        for token in string.gmatch(line, "[^%s]+") do
-            table.insert(report, tonumber(token))
-        end
-
-        if table.difference_not_in_bounds_index(report, -3, -1) == -1 then
-            n_safe = n_safe + 1
-        elseif table.difference_not_in_bounds_index(report, 1, 3) == -1 then
+        if is_safe(line_to_report(line)) then
             n_safe = n_safe + 1
         end
-
     end
 
     print(n_safe)
@@ -46,18 +52,12 @@ function part2()
     io.input("./day02.txt")
     n_safe = 0
     for line in io.lines() do
-        report = {}
-        for token in string.gmatch(line, "[^%s]+") do
-            table.insert(report, tonumber(token))
-        end
+        report = line_to_report(line)
 
         for i, num in ipairs(report) do
             report_temp = table.copy(report)
             table.remove(report_temp, i)
-            if table.difference_not_in_bounds_index(report_temp, -3, -1) == -1 then
-                n_safe = n_safe + 1
-                break
-            elseif table.difference_not_in_bounds_index(report_temp, 1, 3) == -1 then
+            if is_safe(report_temp) then
                 n_safe = n_safe + 1
                 break
             end
@@ -68,4 +68,3 @@ end
 
 part1()
 part2()
-
