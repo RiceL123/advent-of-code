ec7f1476df493d8edf376773f6df59e415e306a1
RiceL123 <ericliao57@gmail.com>
2024-12-04 01:55:40 +1100

    grep -o my beloved

---
+++ 2024/day03.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# part1
+cat day03.txt | grep -Eo 'mul\([[:digit:]]+,[[:digit:]]+\)' | sed -r 's/mul\(([[:digit:]]+),([[:digit:]]+)\)/\1*\2/g' | xargs | sed -r 's/ /+/g' | bc 
+
+# part2
+echo $(cat day03.txt) | perl -pe "s/don\'t\(\).*?do?\(\)|(don't\(\).*?$)//g" | grep -Eo 'mul\([[:digit:]]+,[[:digit:]]+\)' | sed -r 's/mul\(([[:digit:]]+),([[:digit:]]+)\)/\1*\2/g' | xargs | sed -r 's/ /+/g' | bc 
