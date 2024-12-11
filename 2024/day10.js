6b3b9979949a7e92c46e89b0f3efad2ce88b27be
RiceL123 <ericliao57@gmail.com>
2024-12-11 11:59:21 +1100

    functional js

--- 2024/day10.js
+++ 2024/day10.js
@@ -1,5 +1,16 @@
 import fs from "fs";
 
+const directions = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+];
+
+Array.prototype.sum = function () {
+  return this.reduce((acc, x) => acc + x, 0);
+};
+
 function file_to_map_and_start_pos(file_path) {
   const start_positions = [];
 
@@ -19,35 +30,30 @@ function file_to_map_and_start_pos(file_path) {
   return [map, start_positions];
 }
 
-function calculate_score(map, pos, visisted, height) {
+function calculate_score(map, pos, visited, height) {
   if (height == 9) {
-    visisted.add(pos.toString());
+    visited.add(pos.toString());
     return 1;
   }
 
-  let score = 0;
-  const directions = [
-    [-1, 0],
-    [0, 1],
-    [1, 0],
-    [0, -1],
-  ];
-  for (const [d_row, d_col] of directions) {
-    const pos_new = [pos[0] + d_row, pos[1] + d_col];
-    if (
-      pos_new[0] >= 0 &&
-      pos_new[0] < map.length &&
-      pos_new[1] >= 0 &&
-      pos_new[1] < map[0].length &&
-      !visisted.has(pos_new.toString()) &&
-      map[pos_new[0]][pos_new[1]] === height + 1
-    ) {
-      score += calculate_score(map, pos_new, visisted, height + 1);
-      visisted.add(pos.toString());
-    }
-  }
-
-  return score;
+  return directions
+    .filter(([d_row, d_col]) => {
+      let [row, col] = [pos[0] + d_row, pos[1] + d_col];
+      return (
+        row >= 0 &&
+        row < map.length &&
+        col >= 0 &&
+        col < map[0].length &&
+        !visited.has([row, col].toString()) &&
+        map[row][col] === height + 1
+      );
+    })
+    .map(([d_row, d_col]) => {
+      const pos_new = [pos[0] + d_row, pos[1] + d_col];
+      visited.add(pos.toString());
+      return calculate_score(map, pos_new, visited, height + 1);
+    })
+    .sum();
 }
 
 function calculate_rating(map, pos, height) {
@@ -55,38 +61,32 @@ function calculate_rating(map, pos, height) {
     return 1;
   }
 
-  let score = 0;
-  const directions = [
-    [-1, 0],
-    [0, 1],
-    [1, 0],
-    [0, -1],
-  ];
-  for (const [d_row, d_col] of directions) {
-    const pos_new = [pos[0] + d_row, pos[1] + d_col];
-    if (
-      pos_new[0] >= 0 &&
-      pos_new[0] < map.length &&
-      pos_new[1] >= 0 &&
-      pos_new[1] < map[0].length &&
-      map[pos_new[0]][pos_new[1]] === height + 1
-    ) {
-      score += calculate_rating(map, pos_new, height + 1);
-    }
-  }
-
-  return score;
+  return directions
+    .filter(([d_row, d_col]) => {
+      let [row, col] = [pos[0] + d_row, pos[1] + d_col];
+      return (
+        row >= 0 &&
+        row < map.length &&
+        col >= 0 &&
+        col < map[0].length &&
+        map[row][col] === height + 1
+      );
+    })
+    .map(([d_row, d_col]) => {
+      const pos_new = [pos[0] + d_row, pos[1] + d_col];
+      return calculate_rating(map, pos_new, height + 1);
+    })
+    .sum();
 }
 
 const [map, start_positions] = file_to_map_and_start_pos("day10.txt");
-const score = start_positions.reduce(
-  (acc, x) => acc + calculate_score(map, x, new Set(), 0),
-  0
-);
+
+const score = start_positions
+  .map((x) => calculate_score(map, x, new Set(), 0))
+  .sum();
 console.log(`part1: ${score}`);
 
-const rating = start_positions.reduce(
-  (acc, x) => acc + calculate_rating(map, x, 0),
-  0
-);
+const rating = start_positions
+  .map((x) => calculate_rating(map, x, 0))
+  .sum();
 console.log(`part2: ${rating}`);
