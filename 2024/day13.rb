e68a2ce5ecba751ebfb4cc05f5a03d2b30c35d1d
RiceL123 <ericliao57@gmail.com>
2024-12-13 21:44:49 +1000

    system of linear equations

--- 2024/day13.rb
+++ 2024/day13.rb
@@ -2,13 +2,12 @@
 
 require 'json'
 
-# Function to parse a section of lines into a machine structure
 def section_to_machine(section_lines)
-  lines = section_lines.split("\n")
+  button_a, button_b, prize = section_lines.split("\n")
 
-  a_vec_components = lines[0].match(/X\+(\d+), Y\+(\d+)/).captures.map(&:to_i)
-  b_vec_components = lines[1].match(/X\+(\d+), Y\+(\d+)/).captures.map(&:to_i)
-  prize_pos = lines[2].match(/X=(\d+), Y=(\d+)/).captures.map(&:to_i)
+  a_vec_components = button_a.match(/X\+(\d+), Y\+(\d+)/).captures.map(&:to_i)
+  b_vec_components = button_b.match(/X\+(\d+), Y\+(\d+)/).captures.map(&:to_i)
+  prize_pos = prize.match(/X=(\d+), Y=(\d+)/).captures.map(&:to_i)
 
   {
     a: a_vec_components,
@@ -20,57 +19,24 @@ end
 A_COST = 3
 B_COST = 1
 
+require 'matrix'
 def cheapest_win(machine, max_presses)
-  greedy_moves = 0
-  lazy_moves = 0
+  # assume there is only one solution to the system of 2 linear equations
+  # machine[:a][0] * a_presses + machine[:b][0] * b_presses = machine[:prize][0]
+  # machine[:a][1] * a_presses + machine[:b][1] * b_presses = machine[:prize][1]
 
-  greedy_vector = [0, 0]
-  lazy_vector = [0, 0]
+  button_coefficients = Matrix[[machine[:a][0], machine[:b][0]], [machine[:a][1], machine[:b][1]]]
+  prize = Matrix[[machine[:prize][0]], [machine[:prize][1]]]
 
-  # Choose greedy and lazy vectors based on cost-efficiency
-  if machine[:a][0]**2 + machine[:a][1]**2 > (A_COST.to_f / B_COST) * (machine[:b][0]**2 + machine[:b][1]**2)
-    greedy_vector = machine[:a]
-    lazy_vector = machine[:b]
-  else
-    greedy_vector = machine[:b]
-    lazy_vector = machine[:a]
+  a_presses, b_presses = (button_coefficients.inverse * prize).to_a.flatten
+
+  if a_presses % 1 == 0 && b_presses % 1 == 0
+    return (a_presses * A_COST + b_presses * B_COST).to_i
   end
 
-  target_prize = machine[:prize].dup
-
-  for _ in 0..max_presses do
-    x_component = target_prize[0].divmod(greedy_vector[0])
-    y_component = target_prize[1].divmod(greedy_vector[1])
-
-    if x_component[0] == y_component[0] && x_component[1] == 0 && y_component[1] == 0
-      greedy_moves += x_component[0]
-      break
-    else
-      target_prize[0] -= lazy_vector[0]
-      target_prize[1] -= lazy_vector[1]
-      lazy_moves += 1
-    end
-
-    # puts target_prize
-    if target_prize[0] <= 0 || target_prize[1] <= 0
-        return 0
-    end
-  end
-
-  # Validate if the moves can reconstruct the prize position
-  if lazy_moves * lazy_vector[0] + greedy_moves * greedy_vector[0] == machine[:prize][0] &&
-     lazy_moves * lazy_vector[1] + greedy_moves * greedy_vector[1] == machine[:prize][1]
-    if machine[:a] == greedy_vector
-      return greedy_moves * A_COST + lazy_moves * B_COST
-    else
-      return greedy_moves * B_COST + lazy_moves * A_COST
-    end
-  end
-
-  0
+  return 0
 end
 
-# Read the file and split it into sections
 sections = File.read('./day13.txt')
 
 part1 = 0
@@ -78,7 +44,6 @@ sections.split("\n\n").each do |section|
   machine = section_to_machine(section)
   part1 += cheapest_win(machine, 100)
 end
-
 puts part1
 
 part2 = 0
@@ -86,8 +51,6 @@ sections.split("\n\n").each do |section|
   machine = section_to_machine(section)
   machine[:prize][0] += 10000000000000
   machine[:prize][1] += 10000000000000
-  puts machine
   part2 += cheapest_win(machine, Float::INFINITY)
 end
-
 puts part2
