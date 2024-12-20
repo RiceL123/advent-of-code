99e59febfd3859931a7d2ac5aa02dca2f45cc2cf
RiceL123 <ericliao57@gmail.com>
2024-12-21 02:25:40 +1000

    nim

--- 2024/day20.nim
+++ 2024/day20.nim
@@ -1,6 +1,4 @@
-#!/usr/bin/env nim
-
-import strutils, deques, sets, tables
+import strutils, deques, sets, tables, sequtils, algorithm
 
 proc get_racetrack(file_path: string): (seq[seq[char]], (int, int), (int, int)) =
     let racetrack = readFile(file_path).split("\n")
@@ -16,32 +14,31 @@ proc get_racetrack(file_path: string): (seq[seq[char]], (int, int), (int, int)) 
             if c == 'E':
                 end_pos = (row, col)
 
-    var mutable_racetrack: seq[seq[char]] = @[]  # mutable racetrack
-    for line in racetrack:
-        mutable_racetrack.add(@line)
+    return (map(racetrack, proc(x: string): seq[char] = @x), start_pos, end_pos);
 
-    return (mutable_racetrack, start_pos, end_pos);
-
-proc bfs(racetrack: seq[seq[char]], start_pos: (int, int), end_pos: (int, int)): seq[(int, int)] =
+proc bfs(racetrack: seq[seq[char]], start_pos: (int, int), end_pos: (int, int)): (seq[(int, int)], Table[(int, int), int]) =
     var
         visited = initHashSet[(int, int)]()
         prev = initTable[(int, int), (int, int)]()
         queue = [start_pos].toDeque
-        shortest_path = @[end_pos]
+        shortest_path = newSeq[(int, int)]()
+        node_weights = initTable[(int, int), int]()
+        picosecs = 0
 
     while queue.len > 0:
         let curr = queue.popFirst
-        # echo curr
         visited.incl(curr)
+        node_weights[curr] = picosecs
+        picosecs += 1;
 
         if curr == end_pos:
-            # backtrack
+            shortest_path.add(end_pos)
             var backtrack = prev[curr]
             while backtrack != start_pos:
                 shortest_path.add(backtrack)
                 backtrack = prev[backtrack]
-
-            return shortest_path
+            shortest_path.add(start_pos)
+            return (reversed(shortest_path), node_weights)
 
         for (d_row, d_col) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
             let neighbour = (curr[0] + d_row, curr[1] + d_col)
@@ -52,50 +49,23 @@ proc bfs(racetrack: seq[seq[char]], start_pos: (int, int), end_pos: (int, int)):
                 prev[neighbour] = curr
                 queue.addLast(neighbour)
 
-    return shortest_path
+    return (reversed(shortest_path), node_weights)
 
 var (racetrack, s_pos, e_pos) = get_racetrack("day20.txt")
+let (shortest_path, node_weights) = bfs(racetrack, s_pos, e_pos)
 
-# try every permutation of cheats that is neighbouring the path?
-# echo s_pos
-# echo e_pos
-
-let shortest_path = bfs(racetrack, s_pos, e_pos)
-
-var cheats = 0
-var table: OrderedTable[int, int]
-
-var visited_cheats = initHashSet[(int, int)]()
-# for each position on the shortest path, add a cheat in NESW
-for (row, col) in shortest_path:
-    # echo row, ", ", col
-
-    for (d_row, d_col) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-        var racetrack_cheat = racetrack
-        let
-            cheat1 = (row + d_row, col + d_col)
-            cheat2 = (row + d_row * 2, col + d_col * 2)
-
-        if cheat2[0] >= 0 and cheat2[0] < racetrack_cheat.len and
-            cheat2[1] >= 0 and cheat2[1] < racetrack_cheat[0].len and
-            not (cheat1 in visited_cheats) and
-            racetrack_cheat[cheat1[0]][cheat1[1]] == '#' and
-            racetrack_cheat[cheat2[0]][cheat2[1]] in ['.' , 'E']:
-
-            racetrack_cheat[cheat1[0]][cheat1[1]] = '1'
-            racetrack_cheat[cheat2[0]][cheat2[1]] = '2'
-
-            visited_cheats.incl(cheat1)
-            visited_cheats.incl(cheat2)
-
-            let cheat_path = bfs(racetrack_cheat, s_pos, e_pos)
-            if shortest_path.len - cheat_path.len >= 100:
-
-                if (shortest_path.len - cheat_path.len in table):
-                    table[shortest_path.len - cheat_path.len] += 1
-                else:
-                    table[shortest_path.len - cheat_path.len] = 1
+proc num_cheats(max_taxicab_dist: int, min_saved_picos: int): int =
+    var cheats = 0
+    for i in 0..<shortest_path.len:
+        let cheat_start = shortest_path[i]
+        for j in (i + 1)..<shortest_path.len:
+            let cheat_end = shortest_path[j]
+            let taxicab_dist = abs(cheat_end[0] - cheat_start[0]) + abs(cheat_end[1] - cheat_start[1])
+            let saved_picos = node_weights[cheat_end] - node_weights[cheat_start] - taxicab_dist
+            if cheat_end != cheat_start and taxicab_dist <= max_taxicab_dist and saved_picos >= min_saved_picos:
                 cheats += 1
 
-echo "part1: ", cheats
+    return cheats
 
+echo "part1: ", num_cheats(2, 100)
+echo "part2: ", num_cheats(20, 100)
